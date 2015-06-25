52d9978ec655f89ef00599e6056546071451a637
Ying Xiong <yxiong@seas.harvard.edu>
2015-06-25 13:01:53 -0400

    Finish datasets_questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,35 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "Number of data points:", len(enron_data)
+print "Number of features:", len(enron_data.itervalues().next())
 
+poi_count = 0
+for p in enron_data.values():
+    if p["poi"]:
+        poi_count += 1
+print "Number of POIs:", poi_count
+
+salary_count = 0
+for p in enron_data.values():
+    if p["salary"] != "NaN":
+        salary_count += 1
+print "Number of people with quantified salary:", salary_count
+
+email_count = 0
+for p in enron_data.values():
+    if p["email_address"] != "NaN":
+        email_count += 1
+print "Number of people with email address:", email_count
+
+no_total_payments_count = 0
+for p in enron_data.values():
+    if p["total_payments"] == "NaN":
+        no_total_payments_count += 1
+print "Number of people with no total payments:", no_total_payments_count
+
+no_total_payments_poi_count = 0
+for p in enron_data.values():
+    if p["poi"] and p["total_payments"] == "NaN":
+        no_total_payments_poi_count += 1
+print "Number of POIs with no total payments:", no_total_payments_poi_count
